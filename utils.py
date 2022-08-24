17740be414b392d9d7a2f25144d6bf3ce508d1b2
sven <s@ono.re>
2022-08-24 20:27:52 +0200

    humanformat function

---
+++ utils.py
@@ -0,0 +1,15 @@
+
+
+class Utils:
+
+    @staticmethod
+    def humanFormat(num, round_to=2):
+        if num <= 0:
+            return 0
+
+        # From https://stackoverflow.com/questions/579310/formatting-long-numbers-as-strings-in-python
+        magnitude = 0
+        while abs(num) >= 1000:
+            magnitude += 1
+            num = round(num / 1000.0, round_to)
+        return '{:.{}f} {}'.format(num, round_to, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
